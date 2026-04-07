06cc3d0089fb6cb8fa4511d12f8df96d168ad5c1
Chen-Gia <chengiacomo007@gmail.com>
2026-04-08 00:11:59 +0200

    Aggiungi un menu per l'iscrizione e accesso

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="menu">
+        <div class="account-container">
+            <form action="Login/iscrizione.php">
+                <button type="submit">Iscriviti</button>
+            </form>
+            <form action="Login/login.php">
+                <button type="submit">Accedi</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
